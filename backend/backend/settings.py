4fdd806f40e4a8342a2bdea8336df1bcc1746a3e
eli <ebezhiasvhili@gmail.com>
2024-12-27 21:52:48 +0400

    connect postgre sql to django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'financeapp',
+        'USER': 'pgadmin_user',
+        'PASSWORD': 'Makaka12',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
+
 }
 
 
