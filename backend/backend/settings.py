ff6d5633ff4ae4d1ab029d45c2452384f2fcfd6b
eli <ebezhiasvhili@gmail.com>
2024-12-30 12:56:31 +0400

    change cors allowed origins in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+     "http://localhost:5173",    # Vite default
+    "http://localhost:3000",    # Common React default
+    "http://localhost:8080",    # Another common dev port
+    "http://127.0.0.1:5173",   # Vite using IP
+    "http://127.0.0.1:3000",   # React using IP
+    "http://localhost:4173",    # Vite preview port
+]
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
